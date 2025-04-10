f08612a7d4d930a775e503c7115835919ac8b8da
Physics-is-awsome <ajcason@outlook.com>
2025-04-10 09:35:35 -0500

    Update Initial_var.F90

--- MHD/Basic/Initial_var.F90
+++ MHD/Basic/Initial_var.F90
@@ -1,5 +1,11 @@
 Module Initial_var
   implicit none
+  ! Public parameters
+  integer, parameter :: Nx = 50, Ny = 50, Nt = 1000   ! Grid diminsions, plus number of time steps
+  real(kind=8), parameter :: Lx = 1.0, Ly = 1.0, dt = 0.001   !Domain size and tme step size
+  real(kind=8), parameter :: Re = 100.0, Rm = 100.0   !Reynold numbers and magnetic reynold numbers
+  real(kind=8), parameter :: dx = Lx / Nx, dy = Ly / Ny   !Grid spacing
+
   contains 
     subroutine initialize_fields(u, v, Bx, By, p)
         real(kind=8), intent(out) :: u(:,:), v(:,:), Bx(:,:), By(:,:), p(:,:)
@@ -15,25 +21,5 @@ Module Initial_var
             end do
         end do
     end subroutine initialize_fields
-    subroutine Parameters(Nx, Ny, Nt, Lx, Ly, dt, Re, R,, dx, dy, u(Nx, Ny), v(Nx, Ny), Bx(Nx, Ny), By(Nx, Ny), p(Nx, Ny), Jz(Nx, Ny), Jy(Nx, Ny), u_new(Nx, Ny), v_new(Nx, Ny), Bx_new(Nx, Ny), By_new(Nx, Ny)
-      ! Parameters
-      integer, parameter :: Nx = 50, Ny = 50           ! Grid dimensions
-      integer, parameter :: Nt = 1000                  ! Number of time steps
-      real(kind=8), parameter :: Lx = 1.0, Ly = 1.0    ! Domain size
-      real(kind=8), parameter :: dt = 0.001            ! Time step size
-      real(kind=8), parameter :: Re = 100.0            ! Reynolds number
-      real(kind=8), parameter :: Rm = 100.0            ! Magnetic Reynolds number
-      real(kind=8), parameter :: dx = Lx / Nx          ! Grid spacing in x
-      real(kind=8), parameter :: dy = Ly / Ny          ! Grid spacing in y
 
-        ! Field arrays
-      real(kind=8) :: u(Nx, Ny), v(Nx, Ny)             ! Velocity components
-      real(kind=8) :: Bx(Nx, Ny), By(Nx, Ny)           ! Magnetic field components
-      real(kind=8) :: p(Nx, Ny)                        ! Pressure field
-      real(kind=8) :: Jx(Nx, Ny), Jy(Nx, Ny)           ! Current density
-      real(kind=8) :: u_new(Nx, Ny), v_new(Nx, Ny)     ! Updated velocity components
-      real(kind=8) :: Bx_new(Nx, Ny), By_new(Nx, Ny)   ! Updated magnetic field components
-
-      integer :: n                                      ! Time step counter
-    end subroutine Parameters
-end Module
+End Module
