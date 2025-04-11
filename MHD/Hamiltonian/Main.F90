ebe3036ca3165a9daad32a635879b0a0633c2173
Physics-is-awsome <ajcason@outlook.com>
2025-04-11 15:58:38 -0500

    Update Main.F90

--- MHD/Hamiltonian/Main.F90
+++ MHD/Hamiltonian/Main.F90
@@ -3,10 +3,17 @@ module mhd_modules
     use, intrinsic :: iso_c_binding
     implicit none
 
+    include 'fftw3.f03'  ! Try this first; if it fails, use manual definitions below
+
+    ! Manual definitions as fallback
+    integer(C_INT), parameter :: FFTW_FORWARD = -1
+    integer(C_INT), parameter :: FFTW_BACKWARD = 1
+    integer(C_INT), parameter :: FFTW_ESTIMATE = 64
+
     ! Parameters
-    integer, parameter :: Nx = 64, Ny = 64  ! Reduced for testing
-    real(C_DOUBLE), parameter :: Lx = 1.0, Ly = 1.0, dt = 0.001, t_max = 1.0  ! Shorter runtime
-    real(C_DOUBLE), parameter :: cv = 1.0  ! Specific heat
+    integer, parameter :: Nx = 64, Ny = 64
+    real(C_DOUBLE), parameter :: Lx = 1.0, Ly = 1.0, dt = 0.001, t_max = 1.0
+    real(C_DOUBLE), parameter :: cv = 1.0
 
     ! Arrays
     complex(C_DOUBLE_COMPLEX), dimension(Nx, Ny) :: rho_k, u_k, v_k, Bx_k, By_k, s_k, H_k
@@ -22,7 +29,6 @@ contains
     subroutine initialize_simulation()
         integer :: i, j
 
-        ! Initialize wave numbers
         do i = 1, Nx
             kx(i) = 2.0 * 3.14159265359 * (i - 1 - Nx/2) / Lx
         end do
@@ -30,7 +36,6 @@ contains
             ky(j) = 2.0 * 3.14159265359 * (j - 1 - Ny/2) / Ly
         end do
 
-        ! Initial conditions (simple)
         rho_x = 1.0 + 0.01 * sin(2.0 * 3.14159265359 * (kx(1:Nx) / Lx)) * cos(2.0 * 3.14159265359 * (ky(1:Ny) / Ly))
         u_x = 0.1 * sin(2.0 * 3.14159265359 * (kx(1:Nx) / Lx))
         v_x = 0.0
@@ -38,11 +43,9 @@ contains
         By_x = 0.0
         s_x = 0.0
 
-        ! Create FFT plans (using FFTW)
         plan_forward = fftw_plan_dft_2d(Nx, Ny, rho_x, rho_k, FFTW_FORWARD, FFTW_ESTIMATE)
         plan_backward = fftw_plan_dft_2d(Nx, Ny, rho_k, rho_x, FFTW_BACKWARD, FFTW_ESTIMATE)
 
-        ! Transform to Fourier space
         call fftw_execute_dft(plan_forward, rho_x, rho_k)
         call fftw_execute_dft(plan_forward, u_x, u_k)
         call fftw_execute_dft(plan_forward, v_x, v_k)
@@ -50,7 +53,6 @@ contains
         call fftw_execute_dft(plan_forward, By_x, By_k)
         call fftw_execute_dft(plan_forward, s_x, s_k)
 
-        ! Normalize
         rho_k = rho_k / (Nx * Ny)
         u_k = u_k / (Nx * Ny)
         v_k = v_k / (Nx * Ny)
@@ -80,31 +82,16 @@ contains
     subroutine evolve_step(t)
         real(C_DOUBLE), intent(inout) :: t
         complex(C_DOUBLE_COMPLEX), dimension(Nx, Ny) :: k1_rho, k2_rho, k3_rho, k4_rho
-        complex(C_DOUBLE_COMPLEX), dimension(Nx, Ny) :: k1_u, k2_u, k3_u, k4_u
-        complex(C_DOUBLE_COMPLEX), dimension(Nx, Ny) :: k1_v, k2_v, k3_v, k4_v
-        complex(C_DOUBLE_COMPLEX), dimension(Nx, Ny) :: k1_Bx, k2_Bx, k3_Bx, k4_Bx
-        complex(C_DOUBLE_COMPLEX), dimension(Nx, Ny) :: k1_By, k2_By, k3_By, k4_By
-        complex(C_DOUBLE_COMPLEX), dimension(Nx, Ny) :: k1_s, k2_s, k3_s, k4_s
 
-        ! Simplified Poisson bracket (dummy for testing)
         call compute_hamiltonian()
 
-        ! RK4 for rho
-        k1_rho = -0.1 * rho_k  ! Dummy evolution (replace with real Poisson bracket)
+        k1_rho = -0.1 * rho_k
         k2_rho = -0.1 * (rho_k + 0.5*dt*k1_rho)
         k3_rho = -0.1 * (rho_k + 0.5*dt*k2_rho)
         k4_rho = -0.1 * (rho_k + dt*k3_rho)
 
         rho_k = rho_k + (dt/6.0) * (k1_rho + 2.0*k2_rho + 2.0*k3_rho + k4_rho)
 
-        ! Similar for other variables (u, v, Bx, By, s)
-        k1_u = -0.1 * u_k
-        k2_u = -0.1 * (u_k + 0.5*dt*k1_u)
-        k3_u = -0.1 * (u_k + 0.5*dt*k2_u)
-        k4_u = -0.1 * (u_k + dt*k3_u)
-        u_k = u_k + (dt/6.0) * (k1_u + 2.0*k2_u + 2.0*k3_u + k4_u)
-
-        ! Update time
         t = t + dt
 
     end subroutine
