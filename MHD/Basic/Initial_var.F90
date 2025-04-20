68571d3242078e9f46036d2999f37fd9bfe51cd8
Physics-is-awsome <ajcason@outlook.com>
2025-04-19 21:34:17 -0500

    Update Initial_var.F90

--- MHD/Basic/Initial_var.F90
+++ MHD/Basic/Initial_var.F90
@@ -5,7 +5,9 @@ Module Initial_var
   real(kind=8), parameter :: Lx = 1.0, Ly = 1.0, dt = 0.001   !Domain size and tme step size
   real(kind=8), parameter :: Re = 100.0, Rm = 100.0   !Reynold numbers and magnetic reynold numbers
   real(kind=8), parameter :: dx = Lx / Nx, dy = Ly / Ny   !Grid spacing
-
+  real(kind=8), parameter :: T= 0.0                       ! Initial Tempeture
+  real(kind=8), parameter :: eta = 0.001                  !Eta, or ...
+  real(kind=8), parameter :: sigma = 1.0d-21              ! Sigma or...
   contains 
     subroutine velocity_fields(u, v, Bx, By, p)
         real(kind=8), intent(out) :: u(:,:), v(:,:), Bx(:,:), By(:,:), p(:,:)
@@ -20,18 +22,18 @@ Module Initial_var
                 p(i, j) = 0.0                    ! Initialize pressure to zero
             end do
         end do
-    end subroutine initialize_fields
+    end subroutine velocity_fields
     ! Initialize fields for tempeture 
-    subroutine heat_fields
-    T = 0.0
-    eta = 0.001
-    sigma = 1.0d-21
-    do i = 1, nx
-      do j = 1, ny
-        ! Initial temperature: Gaussian blob (actual temperature T)
-        T(i,j) = exp(-((i*dx-0.5*Lx)**2 + (j*dy-0.5*Ly)**2)/(0.1**2))
-        ! Magnetic field: Vary Bx for non-zero Jz
-        Bx(i,j) = 0.1 * sin(2*3.14159*i*dx/Lx)
+    subroutine heat_fields(T, eta, sigma)
+    real(kind=8), intent(out) :: T(:,:)
+    integer :: i, j
+      do i = 1, nx
+        do j = 1, ny
+          ! Initial temperature: Gaussian blob (actual temperature T)
+          T(i,j) = exp(-((i*dx-0.5*Lx)**2 + (j*dy-0.5*Ly)**2)/(0.1**2))
+          ! Magnetic field: Vary Bx for non-zero Jz
+          Bx(i,j) = 0.1 * sin(2*3.14159*i*dx/Lx)
+        end do
       end do
-    end do
+    end subroutine heat_fields
 End Module
