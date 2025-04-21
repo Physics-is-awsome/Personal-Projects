b95765f1c3ae922fb621b06b73843536676f9452
Physics-is-awsome <ajcason@outlook.com>
2025-04-21 16:11:56 -0500

    Update MHD_Module.F90

--- MHD/Basic/MHD_Module.F90
+++ MHD/Basic/MHD_Module.F90
@@ -124,7 +124,7 @@ module mhd_module
         do i = 2, nx-1
             do j = 2, ny-1
                 T_new(i,j) = T(i,j) + dt * (kappa * laplacian(T, i, j, dx, dy) + &
-                                                                        Q(i,j) + &
+                                                    Q(i,j) + &
                                                     radiative_loss(T, i, j, sigma))
                 ! Prevent negative temperatures
                 if (T_new(i,j) < 0.0) T_new(i,j) = 0.0
