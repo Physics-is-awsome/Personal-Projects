bf9cd33a378b6e81cb788c4c12b7235e12caa545
Physics_is_awesome <ajcason@outlook.com>
2025-05-25 17:51:30 -0500

    Update EB.py

--- PSC/EB.py
+++ PSC/EB.py
@@ -1,13 +1,9 @@
-
 from slack_sdk import WebClient
 import os
 
 SLACK_TOKEN = os.getenv("SLACK_TOKEN", "your-slack-bot-token")
 client = WebClient(token=SLACK_TOKEN)
 
-# Test API connection
-response = client.api_test()
-if response["ok"]:
-    print("Slack API connection successful!")
-else:
-    print(f"Error: {response['error']}")
+response = client.conversations_list()
+for channel in response["channels"]:
+    print(f"Channel: {channel['name']}, ID: {channel['id']}")
