a1ed1177eb954967e7729f7e73595b81baa1ed10
Physics_is_awesome <ajcason@outlook.com>
2025-05-27 11:52:11 -0500

    Update Visual.py

--- Fortran/Death_star/Visual.py
+++ Fortran/Death_star/Visual.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 data = np.loadtxt('particle_positions.dat')
-n = (data.shape[1] - 1) // 2  # Number of particles
+n = (data.shape[1] - 2) // 3  # Number of particles
 fig, ax = plt.subplots()
 ax.set_xlim(-6, 6)
 ax.set_ylim(-6, 6)
@@ -14,9 +14,12 @@ def init():
     return points,
 
 def animate(i):
-    x = data[i, 1::2]
-    y = data[i, 2::2]
+    n_active = int(data[i, 1])
+    active = data[i, 2+2*n:2+3*n].astype(bool)
+    x = data[i, 2:2+2*n:2][active]
+    y = data[i, 3:3+2*n:2][active]
     points.set_data(x, y)
+    ax.set_title(f'Time: {data[i, 0]:.2f}, Active Particles: {n_active}')
     return points,
 
 ani = animation.FuncAnimation(fig, animate, init_func=init, frames=data.shape[0], interval=50, blit=True)
