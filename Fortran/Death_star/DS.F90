e7c22c700ef5e4fbfe7a51eb97eeedfae6d65c58
Physics_is_awesome <ajcason@outlook.com>
2025-05-27 19:28:23 -0500

    Update DS.F90

--- Fortran/Death_star/DS.F90
+++ Fortran/Death_star/DS.F90
@@ -1,36 +1,39 @@
 program planet_destruction
   implicit none
-  integer, parameter :: nr = 200, nt = 100  ! Increased resolution
-  real, parameter :: r_planet = 6.371e6     ! Planet radius (m)
-  real, parameter :: t_max = 10.0           ! Simulation time (s)
-  real, parameter :: rho_0 = 3000.0         ! Planet density (kg/m^3)
-  real, parameter :: beam_energy = 1.0e20   ! Beam energy (J)
-  real, parameter :: sigma = 1.0e5          ! Beam width (m)
-  real, parameter :: G = 6.67430e-11       ! Gravitational constant (m^3 kg^-1 s^-2)
-  real, parameter :: M_planet = 5.972e24    ! Earth-like planet mass (kg)
-  integer, parameter :: output_freq = 10     ! Output every 10 steps
-  real, parameter :: cfl = 0.5              ! CFL number for stability
+  integer, parameter :: nr = 200, nt = 100
+  real, parameter :: r_planet = 6.371e6
+  real, parameter :: t_max = 10.0
+  real, parameter :: rho_0 = 3000.0
+  real, parameter :: beam_energy = 1.0e20
+  real, parameter :: sigma = 1.0e5
+  real, parameter :: G = 6.67430e-11
+  real, parameter :: M_planet = 5.972e24
+  integer, parameter :: output_freq = 10
+  real, parameter :: cfl = 0.5
+  real, parameter :: cs_max_limit = 1.0e5  ! Max sound speed (m/s, ~100 km/s)
+  real, parameter :: rho_min = 1.0e-3      ! Min density (kg/m^3)
+  real, parameter :: dt_min = 1.0e-6       ! Min time step (s)
 
   real :: r(nr), theta(nt), dr, dtheta
-  real :: rho(nr, nt), u(nr, nt), v(nr, nt), e(nr, nt)  ! Density, velocities, energy
-  real :: p(nr, nt), cs(nr, nt)                        ! Pressure, sound speed
-  real :: grav_acc(nr)                                 ! Gravitational acceleration
+  real :: rho(nr, nt), u(nr, nt), v(nr, nt), e(nr, nt)
+  real :: p(nr, nt), cs(nr, nt)
+  real :: grav_acc(nr)
   integer :: i, j, step_count, ierr
-  real :: t, r2, q, cs_max, dt_cfl
+  real :: t, dt, r2, q, cs_max, dt_cfl
   character(len=20) :: filename
-  real :: dt = 1.0e-3
+
   ! Initialize grid
   dr = r_planet / real(nr - 1)
   dtheta = 3.14159 / real(nt - 1)
   do i = 1, nr
     r(i) = (i - 1) * dr
-    grav_acc(i) = -G * M_planet / max(r(i)**2, 1.0e3**2)  ! Gravity, avoid r=0
+    grav_acc(i) = -G * M_planet / max(r(i)**2, 1.0e3**2)
     do j = 1, nt
       theta(j) = (j - 1) * dtheta
       rho(i, j) = rho_0
       u(i, j) = 0.0
       v(i, j) = 0.0
-      e(i, j) = 1.0e6  ! Small initial energy to set sound speed
+      e(i, j) = 1.0e6
       p(i, j) = 0.0
       cs(i, j) = 0.0
     end do
@@ -39,6 +42,7 @@ program planet_destruction
   ! Initialize time and step counter
   t = 0.0
   step_count = 0
+  dt = 1.0e-3
 
   ! Time loop
   do while (t < t_max)
@@ -48,59 +52,58 @@ program planet_destruction
     cs_max = 0.0
     do i = 1, nr
       do j = 1, nt
-        ! Simplified Tillotson-like EOS: cs = sqrt(gamma * p / rho)
-        cs(i, j) = sqrt(1.4 * max(p(i, j), 1.0e-10) / max(rho(i, j), 1.0e-10))
+        p(i, j) = (1.4 - 1.0) * rho(i, j) * e(i, j)  ! Update pressure first
+        cs(i, j) = sqrt(1.4 * max(p(i, j), 1.0e-10) / max(rho(i, j), rho_min))
+        cs(i, j) = min(cs(i, j), cs_max_limit)  ! Cap sound speed
         cs_max = max(cs_max, cs(i, j))
       end do
     end do
-    dt_cfl = cfl * min(dr, r_planet * dtheta) / cs_max
-    dt = min(dt, dt_cfl)  ! Use smallest of initial or CFL time step
+    dt_cfl = cfl * min(dr, r_planet * dtheta) / max(cs_max, 1.0e-10)
+    dt = max(min(dt, dt_cfl), dt_min)  ! Enforce min time step
 
     ! Apply plasma beam energy
     do i = 1, nr
       do j = 1, nt
         r2 = (r(i) - r_planet)**2 + (theta(j) * r_planet)**2
         q = (beam_energy / (2.0 * 3.14159 * sigma**2)) * exp(-r2 / (2.0 * sigma**2))
-        e(i, j) = e(i, j) + q * dt / max(rho(i, j), 1.0e-10)
+        e(i, j) = e(i, j) + q * dt / max(rho(i, j), rho_min)
       end do
     end do
 
-    ! Update hydrodynamics (with gravity)
+    ! Update hydrodynamics
     do i = 2, nr - 1
       do j = 2, nt - 1
-        ! Simplified Tillotson-like EOS (approximate)
-        p(i, j) = (1.4 - 1.0) * rho(i, j) * e(i, j)  ! Replace with better EOS later
-        ! Momentum equations with gravity
-        u(i, j) = u(i, j) - dt * (p(i+1, j) - p(i-1, j)) / (2.0 * dr * rho(i, j)) &
+        p(i, j) = (1.4 - 1.0) * rho(i, j) * e(i, j)
+        u(i, j) = u(i, j) - dt * (p(i+1, j) - p(i-1, j)) / (2.0 * dr * max(rho(i, j), rho_min)) &
                   + dt * grav_acc(i)
-        v(i, j) = v(i, j) - dt * (p(i, j+1) - p(i, j-1)) / (2.0 * dtheta * rho(i, j))
-        ! Continuity equation
+        v(i, j) = v(i, j) - dt * (p(i, j+1) - p(i, j-1)) / (2.0 * dtheta * max(rho(i, j), rho_min))
         rho(i, j) = rho(i, j) - dt * rho(i, j) * &
                     ((u(i+1, j) - u(i-1, j)) / (2.0 * dr) + &
                      (v(i, j+1) - v(i, j-1)) / (2.0 * dtheta))
+        rho(i, j) = max(rho(i, j), rho_min)  ! Prevent negative density
       end do
     end do
 
-    ! Apply reflective boundary conditions (simple)
+    ! Apply reflective boundary conditions
     do j = 1, nt
-      rho(1, j) = rho(2, j)
-      u(1, j) = -u(2, j)  ! Reflect velocity
+      rho(1, j) = max(rho(2, j), rho_min)
+      u(1, j) = -u(2, j)
       v(1, j) = v(2, j)
       e(1, j) = e(2, j)
       p(1, j) = p(2, j)
-      rho(nr, j) = rho(nr-1, j)
+      rho(nr, j) = max(rho(nr-1, j), rho_min)
       u(nr, j) = -u(nr-1, j)
       v(nr, j) = v(nr-1, j)
       e(nr, j) = e(nr-1, j)
       p(nr, j) = p(nr-1, j)
     end do
     do i = 1, nr
-      rho(i, 1) = rho(i, 2)
+      rho(i, 1) = max(rho(i, 2), rho_min)
       u(i, 1) = u(i, 2)
       v(i, 1) = -v(i, 2)
       e(i, 1) = e(i, 2)
       p(i, 1) = p(i, 2)
-      rho(i, nt) = rho(i, nt-1)
+      rho(i, nt) = max(rho(i, nt-1), rho_min)
       u(i, nt) = u(i, nt-1)
       v(i, nt) = -v(i, nt-1)
       e(i, nt) = e(i, nt-1)
@@ -109,7 +112,7 @@ program planet_destruction
 
     ! Output data
     if (mod(step_count, output_freq) == 0) then
-      write(filename, '(A, I4.4, A)') 'output_t', step_count / output_freq, '.dat'
+      write(filename, '(A10, I4.4, A4)') 'output_t', step_count / output_freq, '.dat'
       open(unit=10, file=filename, status='replace', iostat=ierr)
       if (ierr /= 0) then
         print *, 'Error opening file: ', trim(filename), ' Error code: ', ierr
@@ -121,12 +124,13 @@ program planet_destruction
         end do
       end do
       close(10)
-      write(*, '(A,A)') 'Wrote file: ', trim(filename)
+      write(*, '(A,A,A,F10.6,A,F10.2)') 'Wrote file: ', trim(filename), ' dt = ', dt, ' cs_max = ', cs_max
     end if
 
     ! Update time
     t = t + dt
-    write(*, '(A,F10.3,A,I6,A,F10.6)') 'Time = ', t, ' Step = ', step_count, ' dt = ', dt
+    write(*, '(A,F10.3,A,I6,A,F10.6,A,F10.2)') 'Time = ', t, ' Step = ', step_count, &
+                                               ' dt = ', dt, ' cs_max = ', cs_max
   end do
 
 end program planet_destruction
