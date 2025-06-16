493207580744aaec0d3ef40d1ccb2b5f2380a8b0
Physics_is_awesome <ajcason@outlook.com>
2025-06-16 08:46:24 -0500

    Update Ast.py

--- Random stuff/Ast.py
+++ Random stuff/Ast.py
@@ -389,8 +389,8 @@ while running:
                     particles.append({
                         "x": asteroid["x"],
                         "y": asteroid["y"],
-                        "dx": (random.random() - 0.5) * 10,
-                        "dy": (random.random() - 0.5) * 10,
+                        "dx": (random.random() - 0.5) * 20,
+                        "dy": (random.random() - 0.5) * 20,
                         "life": 30
                     })
                 if asteroid["radius"] > ASTEROID_SIZES[2]:
